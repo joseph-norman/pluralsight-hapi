9bcc152a41421ef51bba8c1be891a7987c757be2
Joe Norman <josephwlnorman@gmail.com>
2015-09-23 23:15:36 +0100

    Removed typo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var Hapi = require('hapi');
 var server = new Hapi.Server();
 
 server.connection({ port: 3030 });
-s
+
 server.start(function() {
   console.log('Server started at:', server.info.uris);
 });
