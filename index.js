2bc10d29dae8599745dfc90c1233eb9c810d63a7
Joe Norman <josephwlnorman@gmail.com>
2015-09-23 23:28:50 +0100

    Updated to json api plugin, basic route, plugin structure, and comments

--- index.js
+++ index.js
@@ -2,9 +2,37 @@
 var Hapi = require('hapi');
 
 var server = new Hapi.Server();
+server.connection({ port: 3000 });
 
-server.connection({ port: 3030 });
-
-server.start(function() {
-  console.log('Server started at:', server.info.uris);
+server.register({
+	register: require('@gar/hapi-json-api'),
+	options: {}
+}, function(err) {
+	if (err) {
+		throw err;
+	}
 });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply({
+        	data: {
+        		id: 'ok',
+        		type: 'response'
+        	}
+        });
+    }
+});
+
+server.start(function () {
+  console.log('Server running at:', server.info.uri);
+});
+
+
+// get [/api/users]
+// get [/api/user/{id}]
+// post [/api/users]
+// put [/api/users]
+// delete [/api/users]
