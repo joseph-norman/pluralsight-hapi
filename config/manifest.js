87ed4dc65b46db566885cd82e190219dac5d58a4
joseph-norman <josephwlnorman@gmail.com>
2015-10-18 19:17:47 +0100

    I'm an idiot

--- config/manifest.js
+++ config/manifest.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = {
   server: {
     connections: [
@@ -9,31 +8,3 @@ module.exports = {
     ]
   }
 }
-=======
-
-const config = require('./config');
-
-const goodOptions = {
-  reporters: [
-    {
-      reporter: require('good-console'),
-      args: [{ response: '*', log: '*', error: '*', response: '*'}]
-    }
-  ]
-}
-
-const manifest = {
-  connections: [
-    {
-      host: config.host,
-      port: config.port,
-      labels: ['api']
-    }
-  ],
-  plugins: {
-    good: goodOptions
-  }
-};
-
-export manifest;
->>>>>>> 3581ae702c1ac48766d0748a52eb4a6e2332b90e
